2187fe59215f2a4760a5006cee9c21329dedae2f
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-05-27 19:53:16 +0200

    Rename modules

---
+++ lib/concourse/resource/ical.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require 'concourse/resource/ical/occurrences'
+require 'concourse/resource/ical/range_parser'
--- lib/ical_resource.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require 'ical_resource/occurrences'
-require 'ical_resource/range_parser'
